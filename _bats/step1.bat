bd95943d4d9a7dd91a8deb1aa2e7e1c69e492445
gitmsx <gitmsx@mail.ru>
2022-11-14 08:33:39 +0300

    Saved on 14.11.2022- 8:33:34,30

---
+++ _bats/step1.bat
@@ -0,0 +1,27 @@
+@echo off
+if not exist ".gitignore" goto :fail_message
+echo.
+echo    GIT ignore GOOD 
+echo.
+echo    Exiting...
+
+echo "# _nw_vd_016_grid1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gitmsx/_nw_vd_016_grid1.git
+git push -u origin main
+
+goto finished
+:fail_message
+echo.
+echo    Unfortunately GIT ignore not found
+echo.
+echo    Exiting...
+timeout 1 >nul
+goto finished        
+
+rem    ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+:finished
+exit
